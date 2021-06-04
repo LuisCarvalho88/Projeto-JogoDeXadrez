eb4a8034626ea348d2ef3182dcabac773eb8a83a
Luis Carvalho <luis_ios@hotmail.com>
2021-06-04 15:58:18 -0300

    Comecando a colocar pecas no tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -19,10 +19,46 @@
             return pecas[linha, coluna];
         }
 
-        public void colocarPeca(Peca p, Posicao pos) // associacao peca tem um tabuleiro
+        public Peca peca(Posicao pos)   //sobrecarga do metodo peça
         {
+            return pecas[pos.linha, pos.coluna]; // matris peça chamando a posiçao linha e a posição coluna
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+        public void colocarPeca(Peca p, Posicao pos) // asssociação
+        {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.linha, pos.coluna] = p; // jogando a peca p la na matriz na posinha linha e coluna
             p.posicao = pos; //agor a apeca p passa a ser pos
+        }
+
+        //metodo que testa se a posição é valida
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            else // else nesse caso é opcional
+            {
+                return true;
+            }
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida");
+            }
         }
     }
 }
